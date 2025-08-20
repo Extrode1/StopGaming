0328fda441f359831d358eaba3d023a5ecc43b85
Wilson Zhou <wilsonzhou56@gmail.com>
2025-08-20 14:19:31 -0400

    added basic structure for quiz.js

--- quiz.js
+++ quiz.js
@@ -1,6 +1,19 @@
-function quizFunction ()
-{
-    var totalScore = 0; 
-    //first question
-    
+function generateQuiz (questions, quizContainer, resultsContainer, submitButton) {
+    function showQuestions (questions, quizContainer) {
+        //set up questions
+    }
+    function showResults (questions, quizContainer, resultsContainer) {
+            //show the results
+
+    }
+
+    //show questions
+    showQuestions(questions, quizContainer, resultsContainer); 
+
+    //show results when user submits results
+    submitButton.onClick = function () {
+        showResults(questions, quizContainer, resultsContainer); 
+    }
+
+
 }
